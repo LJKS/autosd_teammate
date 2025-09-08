f46d3c97828d65d2cb84aa87211715181543661c
ihuesing <ihuesing@uos.de>
2025-09-08 16:55:58 +0200

    -

--- workflow.py
+++ workflow.py
@@ -11,7 +11,8 @@ def experiment_cycle(theorist, experimentalist, experiment_runner, current_state
     return state
 
 def workflow(theorist, experimentalist, experiment_runner, initial_state, cycles, burn_in):
-    """Run a workflow between a theorist and an experimentalist.
+    """
+    Run a workflow between a theorist and an experimentalist.
     """
 
     #TODO: Prepate the workflow cycle
